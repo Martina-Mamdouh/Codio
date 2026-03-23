5a232fa6f69d52c32addbbbca38a7a20243c1c48
Pavly <pavlysamuelatef@gmail.com>
2026-03-23 11:55:00 +0200

    Update vercel build file

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Download newer Flutter (has Dart 3.8+)
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.0-stable.tar.xz
-tar xf flutter_linux_3.27.0-stable.tar.xz
+# Fix git ownership issue
+git config --global --add safe.directory '*'
+
+# Download NEW Flutter (important: Dart 3.8+)
+curl -L -o flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.29.0-stable.tar.xz
+tar xf flutter.tar.xz
 
 export PATH="$PATH:`pwd`/flutter/bin"
 
 flutter doctor
 flutter config --enable-web
 
+# Clean install deps
+flutter clean
 flutter pub get
+
+# Build admin entry
 flutter build web -t lib/main_admin.dart

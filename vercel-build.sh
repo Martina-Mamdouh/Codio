fd8b8bec14a285386269497a5d299ffe0b8442ef
Pavly <pavlysamuelatef@gmail.com>
2026-03-23 11:48:28 +0200

    Update vercel build file

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# install flutter
-git clone https://github.com/flutter/flutter.git --depth 1
+# Clone full Flutter repo (NOT shallow)
+git clone https://github.com/flutter/flutter.git
+cd flutter
+
+# Checkout stable version (important!)
+git checkout 3.24.0  # or latest stable
+
+cd ..
+
 export PATH="$PATH:`pwd`/flutter/bin"
 
+flutter doctor
 flutter config --enable-web
+
+# Clean + get packages
+flutter clean
 flutter pub get
 
-# 👇 build admin entry
+# Build your admin app
 flutter build web -t lib/main_admin.dart
